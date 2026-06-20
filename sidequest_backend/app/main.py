76f721450980cd3d759d6ab9baeb8465af258e76
Nano <crakernano@gmail.com>
2026-06-20 23:45:01 +0200

    feat: Endpoints de CRUD para planes

--- sidequest_backend/app/main.py
+++ sidequest_backend/app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from fastapi_prometheus_exporter import PrometheusExporterMiddleware
+
+#import logging
+#import logging.config
+from app.db.session import engine
+from app.models import plan as models
 from app.api.api_v1.api import api_router
 
 
@@ -7,11 +13,19 @@ def create_app() -> FastAPI:
     app.include_router(api_router, prefix="/api/v1")
     return app
 
-
 app = create_app()
+
+models.Base.metadata.create_all(bind=engine)
+
+#logging.config.fileConfig("./app/core/logging.conf")
+PrometheusExporterMiddleware.setup(
+    app=app,
+    metrics_path="/metrics",
+    ignore_paths=["/healthz", "/metrics"],
+)
 
 
 if __name__ == "__main__":
     import uvicorn
-
+    #logging.info("Arrancando SideQuest Backend")
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
